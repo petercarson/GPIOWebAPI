3771d7ff9009180e48eb0b26d33b9d9b2e5ff6c6
petercarson <peter@envisionit.com>
2020-11-03 16:23:36 -0500

    Add files via upload

---
+++ Controllers/GPIOController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Device.Gpio;
+
+namespace GPIOWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GPIOController : ControllerBase
+    {
+
+        // GET: api/Heating
+        [HttpGet]
+        public GPIO[] Get()
+        {
+            return GPIOArray.gpioArray;
+        }
+
+        // GET: api/Heating/5
+        [HttpGet("{id}", Name = "Get")]
+        public GPIO Get(int id)
+        {
+            return GPIOArray.gpioArray[id - 1];
+        }
+
+        // PUT: api/Heating/5
+        [HttpPut("{id}")]
+        public void Put(int id, int value)
+        {
+            GPIOArray.gpioArray[id - 1].Value = value;
+        }
+
+    }
+}
---
+++ GPIOArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Device.Gpio;
+
+namespace GPIOWebAPI
+{
+    internal static class GPIOArray
+    {
+        public const bool EMULATOR = true;
+
+        public static GPIO[] gpioArray = new GPIO[]
+                {
+                    new GPIO(1, 13, "HW Tank", PinMode.Output, 0),
+                    new GPIO(2, 21, "Rads", PinMode.Output, 0),
+                    new GPIO(3, 26, "Floor Heat", PinMode.Output, 0),
+                    new GPIO(4, 27, "Ensuite Return", PinMode.Output, 0)
+                };
+
+        // public static GpioController gpioController;
+
+        public static void InitializeGPIO()
+        {
+            if (!GPIOArray.EMULATOR)
+            {
+                // GPIOArray.gpioController = new GpioController();
+            }
+        }
+    }
+}
