b424824e8e44e4ea4a7e6f22af7436751956f0fb
pcarson <peter@envisionit.com>
2022-01-01 11:55:56 -0500

    Refreshed from new project to get local debugging working.

--- HeatingArray.cs
+++ HeatingArray.cs
@@ -7,40 +7,42 @@ using System.Timers;
 
 namespace GPIOWebAPI
 {
-    internal static class HeatingArray
-    {
-      public const int BASEMENT_FLOOR = 0;
-      public const int MASTER_ENSUITE = 1;
-      public const int KATE_BEDROOM = 2;
-      public const int KATE_BATHROOM = 3;
-      public const int MEDIA_ROOM = 4;
-
-      public static Heating[] heatingArray;
-      private static System.Timers.Timer heatingTimer;
-
-
-  public static void InitializeHeating()
+  internal static class HeatingArray
   {
-    heatingArray = new Heating[]
-      {
+    public const int BASEMENT_FLOOR = 0;
+    public const int MASTER_ENSUITE = 1;
+    public const int KATE_BEDROOM = 2;
+    public const int KATE_BATHROOM = 3;
+    public const int MEDIA_ROOM = 4;
+    public const int BASEMENT_BATHROOM = 5;
+
+    public static Heating[] heatingArray;
+    private static System.Timers.Timer heatingTimer;
+
+
+    public static void InitializeHeating()
+    {
+      heatingArray = new Heating[]
+        {
       new Heating(BASEMENT_FLOOR + 1, "Basement Floor", 24, 25, true),
       new Heating(MASTER_ENSUITE + 1, "Master Ensuite", 24, 25, true),
       new Heating(KATE_BEDROOM + 1, "Kate Bedroom", 24, 25, false),
       new Heating(KATE_BATHROOM + 1, "Kate Bathroom", 24, 25, false),
-      new Heating(MEDIA_ROOM + 1, "Media Room", 24, 25, false)
-      };
+      new Heating(MEDIA_ROOM + 1, "Media Room", 24, 25, false),
+      new Heating(BASEMENT_BATHROOM + 1, "Basement Bathroom", 24, 22, false)
+        };
 
-    // Create a timer with a two second interval.
-    heatingTimer = new System.Timers.Timer(2000);
-    // Hook up the Elapsed event for the timer. 
-    heatingTimer.Elapsed += OnTimedEvent;
-    heatingTimer.AutoReset = true;
-    heatingTimer.Enabled = true;
-    // OnTimedEvent(null, null);
-  }
+      // Create a timer with a two second interval.
+      heatingTimer = new System.Timers.Timer(2000);
+      // Hook up the Elapsed event for the timer. 
+      heatingTimer.Elapsed += OnTimedEvent;
+      heatingTimer.AutoReset = true;
+      heatingTimer.Enabled = true;
+      // OnTimedEvent(null, null);
+    }
 
-  private static void OnTimedEvent(Object source, ElapsedEventArgs e)
-  {
+    private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+    {
       // Start by assuming that all the pumps will be off (High) unless one of the systems needs them
       int valuePumpHWTank = 1,
         valuePumpFloorHeat = 1,
@@ -65,23 +67,26 @@ namespace GPIOWebAPI
       }
 
       // Basement thermostat is calling for heat
-      if (heatingArray[HeatingArray.BASEMENT_FLOOR].CurrentTemperature < heatingArray[HeatingArray.BASEMENT_FLOOR].SetTemperature)
+      if (heatingArray[HeatingArray.BASEMENT_FLOOR].Enabled)
       {
-        // Basement floor needs the hot water tank and floor heat pumps
-        valuePumpHWTank = 0;
-        valuePumpFloorHeat = 0;
-        // Open the solenoid for the basement floor heat
-        GPIOArray.gpioArray[GPIOArray.SOLENOID_BASEMENT1].Value = 0;
-        GPIOArray.gpioArray[GPIOArray.SOLENOID_BASEMENT2].Value = 0;
-        GPIOArray.gpioArray[GPIOArray.SOLENOID_BASEMENT3].Value = 0;
-      }
-      // No heat needed for the basement floor
-      else
-      {
-        // Close the solenoid for the basement floor heat
-        GPIOArray.gpioArray[GPIOArray.SOLENOID_BASEMENT1].Value = 1;
-        GPIOArray.gpioArray[GPIOArray.SOLENOID_BASEMENT2].Value = 1;
-        GPIOArray.gpioArray[GPIOArray.SOLENOID_BASEMENT3].Value = 1;
+        if (heatingArray[HeatingArray.BASEMENT_FLOOR].CurrentTemperature < heatingArray[HeatingArray.BASEMENT_FLOOR].SetTemperature)
+        {
+          // Basement floor needs the hot water tank and floor heat pumps
+          valuePumpHWTank = 0;
+          valuePumpFloorHeat = 0;
+          // Open the solenoid for the basement floor heat
+          GPIOArray.gpioArray[GPIOArray.SOLENOID_BASEMENT1].Value = 0;
+          GPIOArray.gpioArray[GPIOArray.SOLENOID_BASEMENT2].Value = 0;
+          GPIOArray.gpioArray[GPIOArray.SOLENOID_BASEMENT3].Value = 0;
+        }
+        // No heat needed for the basement floor
+        else
+        {
+          // Close the solenoid for the basement floor heat
+          GPIOArray.gpioArray[GPIOArray.SOLENOID_BASEMENT1].Value = 1;
+          GPIOArray.gpioArray[GPIOArray.SOLENOID_BASEMENT2].Value = 1;
+          GPIOArray.gpioArray[GPIOArray.SOLENOID_BASEMENT3].Value = 1;
+        }
       }
 
       // Kate's rad is enabled
--- Models/Heating.cs
+++ Models/Heating.cs
@@ -3,64 +3,75 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.RegularExpressions;
 
 namespace GPIOWebAPI
 {
-    public class Heating
+  public class Heating
+  {
+    private float currentTemperature;
+
+    public Heating(int id, string name, float currentTemperature, float setTemperature, bool enabled)
     {
-        private int currentTemperature;
-
-        public Heating(int id, string name, int currentTemperature, int setTemperature, bool enabled)
-        {
-            Id = id;
-            Name = name;
-            CurrentTemperature = currentTemperature;
-            SetTemperature = setTemperature;
-            Enabled = enabled;
-        }
-
-        public int Id { get; set; }
-
-        public string Name { get; set; }
-
-        public int CurrentTemperature
-        {
-            get 
-            {
-                switch (Id - 1)
-                {
-                    case HeatingArray.BASEMENT_FLOOR:
-                        if (GPIOArray.gpioArray[GPIOArray.THERMOSTAT_BASEMENT].Value == 0)
-                        {
-                            currentTemperature = 24;
-                        }
-                        else
-                        {
-                            currentTemperature = 26;
-                        }
-                        break;
-
-                    case HeatingArray.MASTER_ENSUITE:
-                        if (GPIOArray.gpioArray[GPIOArray.THERMOSTAT_MASTER_ENSUITE].Value == 0)
-                        {
-                            currentTemperature = 24;
-                        }
-                        else
-                        {
-                            currentTemperature = 26;
-                        }
-                        break;
-
-                    default:
-                        break;
-                }
-                return currentTemperature; 
-            }
-            set { currentTemperature = value; }
-        }
-
-        public int SetTemperature { get; set; }
-
-        public bool Enabled { get; set; }
+      Id = id;
+      Name = name;
+      CurrentTemperature = currentTemperature;
+      SetTemperature = setTemperature;
+      Enabled = enabled;
     }
+
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public float CurrentTemperature
+    {
+      get
+      {
+        switch (Id - 1)
+        {
+          case HeatingArray.BASEMENT_FLOOR:
+            if (GPIOArray.gpioArray[GPIOArray.THERMOSTAT_BASEMENT].Value == 0)
+            {
+              currentTemperature = 24;
+            }
+            else
+            {
+              currentTemperature = 26;
+            }
+            break;
+
+          case HeatingArray.MASTER_ENSUITE:
+            if (GPIOArray.gpioArray[GPIOArray.THERMOSTAT_MASTER_ENSUITE].Value == 0)
+            {
+              currentTemperature = 24;
+            }
+            else
+            {
+              currentTemperature = 26;
+            }
+            break;
+
+          case HeatingArray.BASEMENT_BATHROOM:
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync("http://192.168.2.165/cm?cmnd=status%2010").Result;
+            string result = response.Content.ReadAsStringAsync().Result;
+            result = Regex.Replace(result, ".*Temperature\":", "");
+            result = Regex.Replace(result, ",\"Humidity.*", "");
+            currentTemperature = float.Parse(result);
+            break;
+
+          default:
+            break;
+        }
+        return currentTemperature;
+      }
+      set { currentTemperature = value; }
+    }
+
+    public float SetTemperature { get; set; }
+
+    public bool Enabled { get; set; }
+  }
 }
